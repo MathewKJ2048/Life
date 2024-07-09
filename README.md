705a364cadcd377b7b4850c6b52000d89c392de8
Mathew Kuthur James <mathewkj2048@gmail.com>
2024-07-09 20:01:11 +0530

    added grid

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Life
 Conway's Game of Life, written as a webapp using HTML5 Canvas and vanilla javascript
+
+## Features:
+
+- step
+- pause/play
+- zoom in and out
+- click to add cell
+- reset
--- script.js
+++ script.js
@@ -1,3 +1,53 @@
-var canvas = document.querySelector("canvas")
+HEIGHT = window.innerHeight * 0.9
+WIDTH = window.innerWidth
+SCALE = 32
 
-console.log(canvas)
+var canvas = document.querySelector("canvas")
+canvas.width = WIDTH
+canvas.height = HEIGHT
+var c = canvas.getContext("2d");
+
+
+class Vector
+{
+	constructor(x,y)
+	{
+		this.x = x
+		this.y = y
+	}
+}
+const camera = new Vector(0,0)
+const mouse = new Vector(undefined,undefined)
+
+window.addEventListener("mousemove",function(event){
+	mouse.x = event.x
+	mouse.y = event.y
+})
+console.log(camera)
+
+
+function animate(){
+	requestAnimationFrame(animate);
+	c.fillStyle = "black"
+	c.fillRect(0,0,WIDTH,HEIGHT)
+
+	for(i=0;i<WIDTH;i+=SCALE)
+	{
+		c.beginPath()
+		c.moveTo(i,0)
+		c.lineTo(i,HEIGHT)
+		c.strokeStyle = "grey"
+		c.stroke()
+	}
+	for(i=0;i<HEIGHT;i+=SCALE)
+	{
+		c.beginPath()
+		c.moveTo(0,i)
+		c.lineTo(WIDTH,i)
+		c.strokeStyle = "grey"
+		c.stroke()
+	}
+	
+}
+animate()
+
